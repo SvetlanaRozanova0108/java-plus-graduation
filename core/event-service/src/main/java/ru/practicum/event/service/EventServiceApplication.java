34ed1877d5f78905f1273600f775c20adffa2e41
Svetlana Rozanova <rozanovasa@yandex.ru>
2025-05-07 22:28:43 +0300

    2-microservices-v4.1

--- core/event-service/src/main/java/ru/practicum/event/service/EventServiceApplication.java
+++ core/event-service/src/main/java/ru/practicum/event/service/EventServiceApplication.java
@@ -3,9 +3,10 @@ package ru.practicum.event.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import ru.practicum.interaction.api.feignClient.client.stat.StatClient;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(clients = {StatClient.class})
 public class EventServiceApplication {
 
     public static void main(String[] args) {
